bbc833769501dc6c005cd8723c654b101e5d85fd
zhangpanpan <zhangpanpan@qianshengqian.com>
2017-09-05 16:56:05 +0800

    fix StringPiece

--- cnet/base/StringPiece.h
+++ cnet/base/StringPiece.h
@@ -74,7 +74,7 @@ public:
     int size() const { return length_; }
     bool empty() const { return length_ == 0; }
     const char* begin() { return ptr_; }
-    const char* end() { return ptr_+length_; }
+    const char* end() const { return ptr_+length_; }
 
     void clear() { ptr_ = NULL; length_ = 0; }
     void set(const char* buffer, int len) { ptr_ = buffer; length_= len; }
